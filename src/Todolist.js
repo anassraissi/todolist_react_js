53e81305b1f66df3aaa8ca16b466027ec2330894
anassraissi <https://github.com/anassraissi/vue_js-.git>
2022-07-31 12:54:54 +0100

    button ajouter

--- src/Todolist.js
+++ src/Todolist.js
@@ -16,14 +16,33 @@ const todos=[...this.state.todos]; //recuperer data
                     todos
             });
 }
-    render(){
+update_task=(event)=>{
+        console.log(event.target.value);
+        this.setState({   // ajouter autre state pour save input value
+          new_task:event.target.value,
+        });
+}
+add_task=()=>{
+  const todos=[...this.state.todos]; //recuperer data
+  todos.push({ 
+          text:this.state.new_task   // affecter input value,
+    });
+    this.setState({
+      todos,
+      new_task:''
+      
+    });
+}
+  render(){
           //componant as class external file
         
         return <div>
                 {this.state.todos.map((todo,index)=><li key={index}>{todo.text+' '} 
-  <button onClick={()=>{this.finish_task(index)}} >Done</button>
-</li>)}
-<input type='text'></input> <button onClick={''} >Add a task</button>
+                <button onClick={()=>{this.finish_task(index)}} >Done</button>
+                </li>)}
+                <input type="text" value={this.state.new_task}  onChange={this.update_task}/>
+
+                <button onClick={()=>{this.add_task()}} >Add a task</button>
         </div> 
     } 
 
