ac4be628b07e869af63123bfaa40cc2dfbf65e77
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2022-08-04 11:28:28 +0100

    select input tips by value and onchange value

--- src/Controlled_component.js
+++ src/Controlled_component.js
@@ -2,13 +2,19 @@ import React,{Component} from 'react';
 class Controlled_component extends Component{
     // controlled component controle inputs inside component
         state={
-                value:'Default'
+                value:'Default',
+                selectvalue:'3'
         }
         setvalue=(event)=>{
                 this.setState({
                     value:event.target.value   //mnin tbadal input 3tiha l value dyalha
                 });
 
+        }
+        change_select_value=(event)=>{
+            this.setState({
+                selectvalue:event.target.value
+            })
         }
 
     render(){
@@ -18,6 +24,13 @@ class Controlled_component extends Component{
             <hr/><hr/>
             <h3>textarea tips on react</h3>
                 <textarea value={this.state.value} onChange={this.setvalue}/>        //declaration
+                <hr/><hr/>
+                 <h3>select tips</h3>
+                 <select value={this.state.selectvalue} onChange={this.change_select_value}> 
+                    <option value={1}>one</option>
+                    <option value={2}>two</option>
+                    <option value={3}>tree</option>
+                 </select>
             </div>
         )
     }
